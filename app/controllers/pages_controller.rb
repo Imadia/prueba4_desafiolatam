978ff3e73db3c8225b0b1170121cea4d33084c3f
Alejandra Diaz <ygthar@gmail.com>
2017-02-14 12:19:22 -0300

    formulario votos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,19 @@ class PagesController < ApplicationController
   end
   def superman
   end
-  def batman_vs_superman
+  def batman_vs_superman 
+  end
+  def votos_batman
+  	VotosBatman.create(name: params[:name], email: params[:mail])
+  	redirect_to pages_batman_vs_superman_path, notice: "El nombre #{params[:a]} ha sido guardado"
+  end
+  def votos_superman
+  	VotosSuperman.create(name: params[:name], email: params[:mail])
+ 	redirect_to pages_batman_vs_superman_path, notice: "El nombre #{params[:a]} ha sido guardado"
+  end
+
+  def votos
+  	@b = VotosBatman.count
+  	@s = VotosSuperman.count  	 	
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root 'pages#batman-vs-superman'
   get 'pages/batman'
   get 'pages/superman'
-  get 'pages/batman-vs-superman'
+  get 'pages/votos'
+  get 'pages/batman-vs-superman', to: 'pages#batman_vs_superman'
+  post 'pages/votos_batman'
+  post 'pages/votos_superman'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
